6b5d10c36638dd0c3c2b47c8717f37797cd2930c
Yanchen Ma <yanchenm@gmail.com>
2018-11-12 18:48:48 -0500

    Update historical scraping

--- web_scraper.py
+++ web_scraper.py
@@ -17,6 +17,11 @@ def scrape_xml(path):
 
     print('Downloading {}...'.format(url))
 
+    file_path = './data/xml/{}'.format(report_name)
+
+    if not os.path.isdir(file_path):
+        os.mkdir(file_path)
+
     with open('./data/xml/{}/{}'.format(report_name, file_name), 'wb') as file:
         file.write(r.content)
 
@@ -29,19 +34,16 @@ def scrape_historic(report_name):
     url = base_url + report_name
     page = requests.get(url)
 
-    file_path = './data/xml/{}'.format(report_name)
-
-    if not os.path.isdir(file_path):
-        os.mkdir(file_path)
-
     soup = BeautifulSoup(page.content, 'html.parser')
     links = soup.find_all('a')
 
     for link in links:
+        # Only get links to reports
         if link.get_text() not in ['Name', 'Last modified', 'Size', 'Description', 'Parent Directory']:
             report_file_name = link['href']
             report_url = url + '/' + report_file_name
 
+            # Match only the most recent version of each report
             pattern = r'[a-zA-Z]+_[a-zA-Z]+_[0-9]+\.xml'
 
             if re.match(pattern, report_file_name) is not None:
